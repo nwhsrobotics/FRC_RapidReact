a7145af238b38e02f21e99201fcabe56ea662309
Neeljt <97802041+Neeljt@users.noreply.github.com>
2022-01-22 14:13:36 -0500

    Added motors to ClimbSubsystem

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -4,9 +4,21 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ClimbSubsystem extends SubsystemBase {
+public class ClimbSubsystem extends SubsystemBase{
+private static final int CAN_ID_RIGHTARM= 20;
+private static final int CAN_ID_LEFTARM= 21;
+private static final int CAN_ID_RIGHTSHOULDER= 22;
+private static final int CAN_ID_LEFTSHOULDER= 23;
+  private CANSparkMax m_rightarmMotor= new CANSparkMax(1, MotorType.kBrushless);
+  private CANSparkMax m_leftarmMotor= new CANSparkMax(1, MotorType.kBrushless);
+  private CANSparkMax m_rightshoulderMotor= new CANSparkMax(1, MotorType.kBrushless);
+  private CANSparkMax m_leftshoulderMotor= new CANSparkMax(1, MotorType.kBrushless);
   /** Creates a new ClimbSubsystem. */
   public ClimbSubsystem() {}
 
