4e655851942ee53732ecf0fa196633d68d6e3872
cwheeler04 <cwheeler20874@gmail.com>
2022-02-05 13:27:18 -0500

    Used Constants.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,8 @@ import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.IndexLoadCommand;
 import frc.robot.commands.IndexShootCommand;
 import frc.robot.commands.IndexUnloadCommand;
+import frc.robot.commands.DriveBackwardCommand;
+import frc.robot.commands.DriveForwardCommand;
 import frc.robot.subsystems.ClimbSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
@@ -40,17 +42,24 @@ public class RobotContainer {
   private final IndexShootCommand m_indexShootCommand = new IndexShootCommand(m_indexSubsystem);
   private final IndexUnloadCommand m_indexUnloadCommand = new IndexUnloadCommand(m_indexSubsystem);
 
+  //private final DriveBackwardCommand m_driveBackwardCommand = new DriveBackwardCommand(m_driveSubsystem);
+
   private final XboxController m_joy0 = new XboxController(0);
   private final JoystickButton joy0_a = new JoystickButton(m_joy0, 1);
+  private final JoystickButton joy0_b = new JoystickButton(m_joy0, 2);
 
+  private final DriveForwardCommand m_driveForwardCommand = new DriveForwardCommand(m_driveSubsystem,m_joy0);
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
   
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    m_driveSubsystem.setDefaultCommand(m_driveForwardCommand);
     // Configure the button bindings
     configureButtonBindings();
   }
-
+  public DriveForwardCommand getM_DriveForwardCommand(){
+    return m_driveForwardCommand;
+  }
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
@@ -60,6 +69,7 @@ public class RobotContainer {
   private void configureButtonBindings() {
     // assign index*Command
     // joy0_a.whenPressed(m_updateIndexPositionCommand);
+    // joy0_b.whenPressed(m_driveForwardCommand);
   }
 
   /**
