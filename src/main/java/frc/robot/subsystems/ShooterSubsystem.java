c68f2d00903a2e436ba92872750a43a19f4ca6fa
nwhsrobotics <jagbotics4638@gmail.com>
2022-02-08 20:32:44 -0500

    Fixed bug with setting shooter speed

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -11,13 +11,14 @@ import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
   private boolean m_flywheelStatus = false;
   private static final int CAN_ID_FLYWHEEL = 1;
   private static final int CAN_ID_FLYWHEEL2 = 2;
+  private static final double RAMP_RATE_SEC = 1.0;
   private CANSparkMax m_flywheelMotor = new CANSparkMax(CAN_ID_FLYWHEEL, MotorType.kBrushless);
   private SparkMaxPIDController m_flywheelpidController;
   private RelativeEncoder m_flywheelencoder;
@@ -25,6 +26,7 @@ public class ShooterSubsystem extends SubsystemBase {
   private SparkMaxPIDController m_flywheel2pidController;
   private RelativeEncoder m_flywheel2encoder;
   private double m_speed_rpm = 0.0;
+  private String[] stringArray = {"Test 1", "Test 2"};
 
 
   private static boolean m_enabled = false;
@@ -38,30 +40,34 @@ public class ShooterSubsystem extends SubsystemBase {
     m_flywheelencoder = m_flywheelMotor.getEncoder();
 
     m_flywheelencoder.setPosition(0);
-    m_flywheelMotor.setIdleMode(IdleMode.kCoast);
+    m_flywheelMotor.setIdleMode(IdleMode.kBrake);
 
     m_flywheelpidController.setP(0.0002);
-    m_flywheelpidController.setI(0.000001);
+    m_flywheelpidController.setI(0.0);
     m_flywheelpidController.setD(0);
     m_flywheelpidController.setIZone(0);
     m_flywheelpidController.setFF(0.000178);
     m_flywheelpidController.setOutputRange(-1.0, 1.0);
+    m_flywheelMotor.setClosedLoopRampRate(RAMP_RATE_SEC);
 
     m_flywheel2pidController = m_flywheel2Motor.getPIDController();
     m_flywheel2encoder = m_flywheel2Motor.getEncoder();
 
     m_flywheel2encoder.setPosition(0);
-    m_flywheel2Motor.setIdleMode(IdleMode.kCoast);
+    m_flywheel2Motor.setIdleMode(IdleMode.kBrake);
 
     m_flywheel2pidController.setP(0.0002);
-    m_flywheel2pidController.setI(0);
+    m_flywheel2pidController.setI(0.0);
     m_flywheel2pidController.setD(0);
     m_flywheel2pidController.setIZone(0);
     m_flywheel2pidController.setFF(0.000178);
     m_flywheel2pidController.setOutputRange(-1.0, 1.0);
+    m_flywheel2Motor.setClosedLoopRampRate(RAMP_RATE_SEC);
 
     m_flywheelpidController.setReference(0.0, ControlType.kVelocity);
     m_flywheel2pidController.setReference(0.0, ControlType.kVelocity);
+    
+    
   }
 
 
@@ -72,11 +78,14 @@ public class ShooterSubsystem extends SubsystemBase {
     if (!m_enabled) {
       return;
     }
+    //m_speed_rpm = SmartDashboard.getNumber("flywheelSpeed (rpm)", 0.0);
+    
+    
 
     //Set moters for m_speed_rpm
     
-    m_flywheelpidController.setReference(-m_speed_rpm, ControlType.kVelocity);
-    m_flywheel2pidController.setReference(m_speed_rpm, ControlType.kVelocity);
+    m_flywheelpidController.setReference(m_speed_rpm, ControlType.kVelocity);
+    m_flywheel2pidController.setReference(-m_speed_rpm, ControlType.kVelocity);
   
   }
 
@@ -92,8 +101,18 @@ public class ShooterSubsystem extends SubsystemBase {
       m_flywheelStatus = true;
     }
     m_speed_rpm = speed_rpm;
+    SmartDashboard.putNumber("flywheelSpeed (rpm)", speed_rpm);
 
     //TODO: Implement the setReference method here to move the motor to the desired speed
   }
+
+  
+  
+  /*
+  public boolean getFlywheelControl() {
+    String[] flywheelModes = {"Flywheel Switcher", "Smart Dashboard Value"};
+    SmartDashboard.putStringArray("Flywheel Mode:", flywheelModes);
+    
+  }*/
 
 }
